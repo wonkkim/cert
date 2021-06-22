0329b18f2578f3f2b14b95c83a8d574fc13e0252
wonk <nons85@gmail.com>
2021-06-22 23:33:24 +0900

    OpenCV, json 추가

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenCvSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mat img = new Mat(200, 300, MatType.CV_8UC3);
+            Cv2.Rectangle(img, new Rect(50, 50, 200, 100), new Scalar(0,0,255), -1);
+            Cv2.ImShow("a", img);
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+        }
+    }
+}
